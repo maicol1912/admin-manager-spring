417e108edaaa71d689db43d1fb5793660b4f8c0a
maicol1912 <maicolarcila34@gmail.com>
2022-10-19 23:06:53 -0500

    Se realizo el mapeo entre 2 tablas de usuario y rol, se utilizo el modulo de spring data Jpa para realizar esto ademas de que se le agregaron estilos a los templates

---
+++ src/main/java/com/example/springData/Controller/indexController.java
@@ -0,0 +1,12 @@
+package com.example.springData.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
